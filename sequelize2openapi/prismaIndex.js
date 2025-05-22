f237382063e86115d61856876b85b8edbc2958e1
hasininjato <hasininjato@voisinconsulting.com>
2025-05-22 14:52:40 +0000

    get default value for prisma model

--- sequelize2openapi/prismaIndex.js
+++ sequelize2openapi/prismaIndex.js
@@ -1,5 +1,6 @@
 const { modelParser } = require("./src/parsers/modelParser");
 const { getSchema } = require('@mrleebo/prisma-ast');
+const fs = require('fs');
 
 const source = `
 // Data source
@@ -17,11 +18,12 @@ generator client {
 model Post {
   id        Int     @id @default(autoincrement())
   // @swag | description: Title of the post | methods: list, item, post, put
-  title     String
+  title     String @unique @default("Untitled") @db.VarChar(100)
   content   String?
   published Boolean @default(false)
   author    User?   @relation(fields:  [authorId], references: [id])
   authorId  Int?
+  createdAt   DateTime @default(now())
 }
 
 model User {
@@ -32,25 +34,37 @@ model User {
 }
 `
 const schemas = getSchema(source);
-
+fs.writeFileSync("../prisma.json", JSON.stringify(schemas, null, 4));
 let models = [];
 for (const schema of Object.values(schemas.list)) {
-    let model = {}
-    if (schema.type === "model") {
-        const sequelizeModel = schema.name;
-        model = {
-            sequelizeModel: sequelizeModel,
-            value: []
-        }
-        for (const property of schema.properties) {
-            console.log(JSON.stringify(property, null, 2));
-            const valueItem = {
-                field: property.name,
-                type: "field",
-
-            }
-        }
-        models.push(model);
-    }
+	let model = {}
+	if (schema.type === "model") {
+		const sequelizeModel = schema.name;
+		model = {
+			sequelizeModel: sequelizeModel,
+			value: []
+		}
+		for (const property of schema.properties) {
+			if (property.type === "field") {
+				const valueItem = {
+					field: property.name,
+					type: "field",
+					object: {
+						type: property.fieldType,
+						allowNull: property.optional,
+					}
+				}
+				property.attributes?.forEach((attr) => {
+					// get default value of the field
+					if (attr.name === "default") {
+						// console.log(attr.args[0])
+						valueItem.object.defaultValue = attr.args[0].value.name ?? attr.args[0].value;
+					}
+				})
+				model.value.push(valueItem);
+			}
+		}
+		models.push(model);
+	}
 }
-console.log(models);
+console.log(JSON.stringify(models, null, 4));
